dbc83b70c873f1ed565a2b013b3bd0df0b45a640
Alan <aas3zc@umsystem.edu>
2024-11-14 21:08:48 -0600

    -Fixed MainActivity

--- app/src/main/java/com/www/xfactor/MainActivity.kt
+++ app/src/main/java/com/www/xfactor/MainActivity.kt
@@ -21,8 +21,6 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        navigateToMapActivity()
-
         // Configure the next button to switch to MainActivity2
         configureNextButton()
         configureRegButton()
